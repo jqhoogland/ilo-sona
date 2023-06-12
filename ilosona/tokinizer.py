ae8757587a4a06f76589e6dff9f4f77f941ff9c9
Jesse Hoogland <jessequinten@gmail.com>
2023-06-12 16:52:55 +0100

    CHG tokinizer

--- ilosona/tokinizer.py
+++ ilosona/tokinizer.py
@@ -143,17 +143,21 @@ DEFAULT_VOCABULARY = [
     "wile",
 ]
 
+VOCABULARY = DEFAULT_VOCABULARY
 LETTERS = list("abcdefghijklmnopqrstuvwxyz" + "abcdefghijklmnopqrstuvwxyz".upper())
-DEFAULT_PUNCTUATION = list(".,?!()[]{}:;\"'\\/-+=~<>`@#$%^&*_")
-NUMBERS = list("1234567890")
-VOCABULARY = DEFAULT_VOCABULARY + DEFAULT_PUNCTUATION + LETTERS + NUMBERS
+DEFAULT_PUNCTUATION = list(".,?!()[]{}:;\"'\\/-+=~<>`@#$%^&*_\n")
+NUMBERS = list("0123456789")
+EXTRA = DEFAULT_PUNCTUATION + LETTERS + NUMBERS
+
 
 class Tokinizer:
-    def __init__(self, punctuation=DEFAULT_PUNCTUATION, vocabulary=DEFAULT_VOCABULARY):
+    def __init__(self, vocabulary=DEFAULT_VOCABULARY, extra=EXTRA):
         self.vocabulary = vocabulary
-        self.punctuation = punctuation
-        self.token_to_id = {token: i for i, token in enumerate(vocabulary)}
+        self.extra = extra
+        self.tokens = vocabulary + extra
+        self.token_to_id = {token: i for i, token in enumerate(self.tokens)}
         self.vocab_size = len(self.vocabulary)
+        self.final_punctuation = ".,;:?!)-_"
 
     def unpack_lists(self, nested_list):
         flattened_list = []
@@ -165,18 +169,28 @@ class Tokinizer:
         return flattened_list
 
     def split(self, text):
-        for char in self.punctuation:
-            text = text.replace(char, " " + char + " ")
-        
-        tokens = text.split()
+        tokens = []
 
-        for token in tokens:
-            if token not in self.vocabulary:
-                warnings.warn(f"{token} not in vocab", UserWarning)
-                
-        return self.unpack_lists(
-            [token if token in self.vocabulary else list(token) for token in tokens]
-        )
+        curr_word = ""
+
+        for c in text:
+            if c in LETTERS:
+                curr_word += c
+                continue
+            elif curr_word in self.vocabulary:
+                tokens.append(curr_word)
+                curr_word = ""
+                continue
+            else:
+                tokens.extend(list(curr_word))
+                curr_word = ""
+            
+            if c in self.tokens or c == " ":
+                tokens.append(c)
+            else: 
+                warnings.warn(f"{c} not in vocab", UserWarning)
+                                
+        return tokens
     
     def tokens_to_ids(self, tokens):
         return [self.token_to_id[token] for token in tokens if token in self.token_to_id]
@@ -204,11 +218,27 @@ class Tokinizer:
         return input_ids
 
     def encode(self, text):
+        """Get a one-hot encoded array"""
         ids = self.encode_ids(text)
         return np.onehot(ids)
     
     def combine(self, words):
-        raise NotImplementedError
+        text = ""
+
+        full_word = ""
+
+        for word in words:
+            if word in self.vocabulary and not full_word:
+                word += " "
+                text += word 
+            elif word in self.final_punctuation and text[-1] == " ":
+                text = text[:-1]
+                text += word 
+            else:
+                full_word += word
+
+
+        return text
 
 
 if __name__ == "__main__":
@@ -216,8 +246,10 @@ if __name__ == "__main__":
         text = f.read()
 
     tokinizer = Tokinizer()
+    tokens = tokinizer.split(text)
 
     print(text)
     print("---")
-    print(tokinizer.split(text))
+    print(tokens)
 
+    print(tokinizer.combine(tokens))
